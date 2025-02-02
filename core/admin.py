502b009fe18d0427ded727ca1ab52a6fa13cf6a7
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-02-02 12:36:58 +0330

    create product model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProductModel
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+admin.site.register(ProductModel, ProductAdmin)    
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ProductModel(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Product Name'))
+    description = models.TextField(verbose_name=_('Product Description'))
+
+    def __str__(self):
+        return f'{self.name}'
